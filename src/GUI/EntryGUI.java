f102fd2c0d97c28abb0da57245ae2fa3759527dd
Sajag <sajag.acharya@gmail.com>
2015-11-28 10:12:11 +0545

    Test commit

---
+++ src/GUI/EntryGUI.java
@@ -0,0 +1,18 @@
+package GUI;
+
+import javax.swing.*;
+
+/**
+ * Created by Sajag on 11/28/2015.
+ */
+public class EntryGUI {
+    private JTextField NameTF;
+
+    public EntryGUI(){
+        initComponents();
+    }
+
+    private void initComponents() {
+
+    }
+}
--- src/Reporter.java
+++ src/Reporter.java
@@ -2,4 +2,13 @@
  * Created by Sajag on 11/27/2015.
  */
 public class Reporter {
+
+    /**
+     * Main Function
+     */
+    public static void main(String args[]){
+        System.out.println("Wazzaaa!");
+
+    }
+
 }
